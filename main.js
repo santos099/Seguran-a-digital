03af1759689e5b4ca01f058b8c60db56346616ab
santos099 <murilo.souza10@escola.pr.gov.br>
2026-06-22 15:13:10 +0000

    aula 5 pt1

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
+
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
        // tamanhoSenha = tamanhoSenha-1;
@@ -13,10 +14,21 @@ function diminuiTamanho(){
     }
     numeroSenha.textContent = tamanhoSenha;
 }
+
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
        // tamanhoSenha = tamanhoSenha+1;
        tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
+}
+
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
 }
